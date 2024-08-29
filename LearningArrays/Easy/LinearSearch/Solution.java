b3f2a2524ca75f3009a185fec0228c974fc6235c
Meenal Phartiyal <meenalphartiyal17@gmail.com>
2024-08-29 14:04:05 +0530

    commit

--- LearningArrays/Easy/LinearSearch/Solution.java
+++ LearningArrays/Easy/LinearSearch/Solution.java
@@ -1,6 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Solution{
+    public static int solution(List<Integer> arr, int target){
+        for(int i=0; i<arr.size(); i++){
+            if(arr.get(i) == target)
+                return i;
+        }
+        return -1;
+    }
     public static void main(String[] args) {
-        
+        List<Integer> arr = new ArrayList<>(Arrays.asList(1,3,9,52,76,32,5,7));
+        System.out.println(solution(arr, 52));
     }
 
 }
